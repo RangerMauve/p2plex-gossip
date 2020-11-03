c789e35c33391445b91ddea6a30da6d35d3e6fae
mauve <ranger@mauve.moe>
2020-11-02 19:50:28 -0500

    Initial progress on implementation

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # p2plex-gossip
 Create a secure (ish) swarm of peers to gossip over P2Plex
+
+**Work in progress**
+
+## How it works
+
+- Get a p2plex instance
+- Get a set of peers you wish to gossip with
+- Set of peers should contain their p2plex public keys as well as their public keys used for signing messages
+- Generate a key to find peers based on the set
+- Optionally provide a nonce to mix into the key and into messages (useful to avoid replay attacks)
+- Find peers for the key
+- Find connections from peers and open streams for the key for them
+- Use abstract-gossip to send and recieve messages from peers
+
+## API
+
+```JavaScript
+const Gossip = require('p2plex-gossip')
+
+const swarm = p2plex()
+
+const peers = new Map()
+
+peers.set(remotePublicKey.toString('hex'), signingPublicKey)
+
+const gossip = await Gossip.create({
+	// P2Plex instance to use, the public key will be used for swarming
+	swarm,
+
+	// List of peers to gossip with
+	// Map of `remotePublicKey` to `signingPublicKey`
+	peers,
+
+	// Pass a nonce to prevent replay attacks
+	// Must be a buffer with unique bytes
+	nonce,
+
+	// Keypair used for signing
+	// Should be compatible with [libsodium_crypto_sign_*](https://doc.libsodium.org/public-key_cryptography/public-key_signatures)
+	keyPair,
+
+	// Alternately you can specify custom signing and verifying functions
+	// Sign a message for your ID
+	async sign(message) { return signature},
+	// Given a peer's publicKey, a message and a signature, verify that it's valid
+	// The `id` will be the value part of the pair inside the `peers` map
+	async verify(id, message, signature) { return isValid},
+	// Pass a custom hash function for generating keys
+	async hashBytes(bytes) { return hash}
+})
+
+// Listen on messages being gossiped in the swarm
+gossip.on('message', (data, from) => {
+	// Saw a message that got broadcasted by `from`
+	// This was cryptographically verified to come from them
+	// Data is the data that the original peer sent out
+})
+
+// Broadcast some data to the swarm
+// `data` must be a Buffer
+// Signed with nonce to avoid replay attacks
+gossip.broadcast(data)
+
+// Sign some data to send out
+const signedData = await gossip.sign(data)
+const {from, signature, nonce, data} = signedData
+
+// Verify that some data you got was signed
+// The `from` ID is the remotePublic key of the peer
+const isValid = await gossip.verify(from, data, nonce, signature)
+```
---
+++ index.js
@@ -0,0 +1,191 @@
+/* eslint camelcase: 0 */
+const EventEmitter = require('events')
+
+const lpstream = require('length-prefixed-stream')
+
+const {
+  crypto_sign_BYTES,
+  crypto_generichash_BYTES,
+
+  crypto_sign_detached,
+  crypto_sign_verify_detached,
+  crypto_generichash
+
+} = require('sodium-universal')
+
+const { AbstractFlood } = require('abstract-flood')
+
+const { Message } = require('./messages')
+
+const NONCE_INDEX_BYTES = 4
+
+class P2PlexGossip extends EventEmitter {
+  constructor ({
+    swarm,
+    peers,
+    nonce = Buffer.from([]),
+
+    ttl,
+    lruSize,
+    messageIndex,
+
+    keyPair,
+
+    sign = (message) => defaultSign(this.keyPair, message),
+    verify = (id, message, signature) => defaultVerify(this.peers, id, message, signature),
+    hashBytes = defaultHashBytes
+  }) {
+    super()
+    this.peers = peers
+    this.swarm = swarm
+    this.nonce = nonce
+    this.keyPair = keyPair
+    this.nonceIndex = 0
+
+    this.connections = new Set()
+    this.channel = null
+
+    const floodOpts = {
+      id: this.id
+    }
+    if (ttl) floodOpts.ttl = ttl
+    if (lruSize) floodOpts.lruSize = lruSize
+    if (messageIndex) floodOpts.messageIndex = messageIndex
+
+    this.flood = new AbstractFlood(floodOpts)
+
+    this._sign = sign
+    this._verify = verify
+    this._hashBytes = hashBytes
+  }
+
+  get id () {
+    return this.swarm.publicKey
+  }
+
+  async init () {
+    this.flood.on('message', async (message) => {
+      try {
+        await this._handleMessage(message)
+      } catch (e) {
+        this.emit('error', e)
+      }
+    })
+
+    this.flood.on('broadcast', (message) => {
+      for (const stream of this.connections) {
+        stream.write(message)
+      }
+    })
+
+    this._onConnection = (peer) => this._handleConnection(peer)
+
+    this.swarm.on('connection', this._onConnection)
+
+    this.channel = await this.generateChannel()
+
+    this.flood.join(this.channel, { announce: true, lookup: true })
+  }
+
+  async generateChannel () {
+    const peerKeys = [...this.peers.keys()].map((key) => Buffer.from(key, 'hex'))
+    const bytes = Buffer.concat(this.nonce, ...peerKeys)
+
+    return this.hashBytes(bytes)
+  }
+
+  async sign (data) {
+    const indexNonce = Buffer.alloc(NONCE_INDEX_BYTES)
+    indexNonce.writeInt32BE(this.nonceIndex++)
+    const nonce = Buffer.concat(indexNonce, this.nonce)
+    const toSign = Buffer.concat(data, nonce)
+
+    const signature = await this._sign(toSign)
+    const from = this.id
+
+    return { from, signature, nonce, data }
+  }
+
+  async verify (from, data, nonce, signature) {
+    const isValidNonce = this.isValidNonce(nonce)
+
+    if (!isValidNonce) return false
+
+    const toSign = Buffer.concat(data, nonce)
+    return this.verify(from, data, toSign)
+  }
+
+  async _handleMessage (message) {
+    const { from, nonce, signature, data } = Message.decode(message)
+
+    const isValid = await this.verify(from, data, nonce, signature)
+
+    if (isValid) {
+      this.emit('message', data, from, nonce, signature)
+    } else {
+      this.emit('error-invalid', data, from, nonce, signature)
+    }
+  }
+
+  async _handleFromPeer (data) {
+    this.flood.handleMessage(data)
+  }
+
+  async _handleConnection (peer) {
+    const peerKey = peer.publicKey.toString('hex')
+    const isPeer = this.peers.has(peerKey)
+    if (!isPeer) return
+    const stream = peer.createSharedStream(this.channel)
+
+    const encode = lpstream.encode()
+    const decode = lpstream.decode()
+
+    decode.on('data', (data) => this._handleFromPeer(data))
+
+    this.connections.add(stream)
+
+    stream.once('end', () => {
+      this.connections.delete(stream)
+    })
+
+    stream.pipe(decode)
+    encode.pipe(stream)
+  }
+
+  isValidNonce (nonce) {
+    return nonce.slice(NONCE_INDEX_BYTES).equals(this.nonce)
+  }
+
+  async broadcast (data) {
+    const { signature, nonce, from } = await this.sign(data)
+    this.flood.broadcast(Message.encode({
+      from,
+      nonce,
+      signature,
+      data
+    }))
+  }
+}
+
+module.exports = P2PlexGossip
+
+function defaultSign (keyPair, message) {
+  const signature = Buffer.alloc(crypto_sign_BYTES)
+
+  crypto_sign_detached(signature, message, keyPair.secretKey)
+
+  return signature
+}
+
+function defaultVerify (peers, id, message, signature) {
+  const publicKey = peers.get(id.toString('hex'))
+
+  return crypto_sign_verify_detached(signature, message, publicKey)
+}
+function defaultHashBytes (bytes) {
+  const hash = Buffer.alloc(crypto_generichash_BYTES)
+
+  crypto_generichash(hash, bytes)
+
+  return hash
+}
